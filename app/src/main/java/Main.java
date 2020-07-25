24bf263ed6754a091ffbc15bc6f593fd63b911f4
tahaviev <tvramil@gmail.com>
2020-07-25 20:32:40 +0300

    log4j with map context example

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,9 +1,16 @@
+import java.util.Map;
+import org.apache.logging.log4j.CloseableThreadContext;
+import org.apache.logging.log4j.LogManager;
+
 public final class Main {
 
     public static void main(final String... args) throws Exception {
-        while (!Thread.interrupted()) {
-            System.out.println("Hello");
-            Thread.sleep(3000);
+        final var metadata = Map.of("key1", "value1", "key2", "value2");
+        try (var context = CloseableThreadContext.putAll(metadata)) {
+            while (!Thread.interrupted()) {
+                LogManager.getLogger().info("hello");
+                Thread.sleep(3000);
+            }
         }
     }
 }
