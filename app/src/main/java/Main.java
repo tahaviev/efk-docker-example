8585a0eb3b2ec91990f5d949b7482d8b8a1cd315
tahaviev <tvramil@gmail.com>
2020-07-27 16:19:51 +0300

    tail source example

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,8 +1,13 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
 public final class Main {
 
     public static void main(final String... args) throws Exception {
+        final var log = Files.createDirectories(Path.of("logs")).resolve("log.txt");
         while (!Thread.interrupted()) {
-            System.out.println("Hello");
+            Files.writeString(log, "hello\n", StandardOpenOption.APPEND, StandardOpenOption.CREATE);
             Thread.sleep(3000);
         }
     }
